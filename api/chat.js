f7a3928cc1c0f804d81f071a55ea6c9d687f84d1
LegendPiecA23 <dasbabli.apd@gmail.com>
2026-06-01 11:17:40 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,1 +1,33 @@
+export default async function handler(req, res) {
 
+    if (req.method !== "POST") {
+        return res.status(405).json({
+            error: "Method not allowed"
+        });
+    }
+
+    try {
+
+        const response = await fetch(
+            `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${process.env.AIzaSyAAVO9GxJ1upY1LTrBizLiiA_nKNxehdJk}`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(req.body)
+            }
+        );
+
+        const data = await response.json();
+
+        return res.status(200).json(data);
+
+    } catch (error) {
+
+        return res.status(500).json({
+            error: error.message
+        });
+
+    }
+}
